ca4ab31ee99249bfea5bb41c4c4cab27f9a6746b
juii <dlwngml480@naver.com>
2025-08-22 14:21:26 +0900

    change rate discount polish

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.Discount.DiscountPolicy;
 import hello.core.Discount.FixDiscountPolicy;
+import hello.core.Discount.RateDiscountPolish;
 import hello.core.Member.MemberRepository;
 import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
@@ -10,10 +12,18 @@ import hello.core.Order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolish();
     }
 }
